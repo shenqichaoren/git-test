53330210c3e49abf69453cb529638ee5c6d9e5fe
shenqichaoren <569894960@qq.com>
2022-06-20 20:47:12 +0800

    first commit

---
+++ src/main/java/com/DD/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.DD.git;
+
+/**
+ * @author shenqichaoren
+ * @create 2022-06-20-20:44
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
